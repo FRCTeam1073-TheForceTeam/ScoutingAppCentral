35b5ac06d7ac9404a8ae46401fdd05c3cc71656f
Ken St.Hilaire <kensthilaire@gmail.com>
2013-02-28 00:02:10 -0500

    moved scoring recalculation function to data model module.
    This change is not yet tested, but isn called from anywhere just yet. so
    it's safe to commit it for now and it will be fully tested when the rest
    of the work is done.

--- CentralApp/src/DataModel.py
+++ CentralApp/src/DataModel.py
@@ -5,6 +5,7 @@ Created on Dec 27, 2011
 @author: ksthilaire
 '''
 
+import os
 import datetime
 from sqlalchemy import create_engine
 from sqlalchemy import schema
@@ -15,6 +16,7 @@ from optparse import OptionParser
 import urllib2
 import json
 
+import DbSession
 import AttributeDefinitions
 
 
@@ -413,6 +415,87 @@ def dump_db_tables(my_db):
     meta = schema.MetaData(my_db)
     meta.reflect()
     meta.drop_all()
+
+def recalculate_scoring(global_config):
+    session = DbSession.open_db_session(global_config['db_name'])
+    competition = global_config['this_competition']
+    if competition == None:
+        raise Exception( 'Competition Not Specified!')
+
+    # Build the attribute definition dictionary from the definitions csv file
+    #attrdef_filename = './config/' + 'AttributeDefinitions-reboundrumble.csv'    
+    attrdef_filename = './config/' + global_config['attr_definitions']    
+    attr_definitions = AttributeDefinitions.AttrDefinitions()
+    attr_definitions.parse(attrdef_filename)
+
+    team_rankings = getTeamsInRankOrder(session, competition)
+    for team_entry in team_rankings:
+        score = calculateTeamScore(session, team_entry.team, competition, attr_definitions)
+        setTeamScore(session, team_entry.team, competition, score)
+    session.commit()
+    dump_database_as_csv_file(session, global_config, attr_definitions, competition)
+
+def dump_database_as_csv_file(session, global_config, attr_definitions, competition=None):
+    
+    if competition == None:
+        competition = global_config['this_competition']
+    
+    # read in the attribute definitions and sort them in the colum order
+    attr_dict = attr_definitions.get_definitions()
+    attr_order = [{} for i in range(len(attr_dict))]
+    
+    try:
+        for key, value in attr_dict.items():
+            attr_order[(int(float(value['Column_Order']))-1)] = value
+            #attr_order[(int(value['Column_Order'])-1)] = value
+    except Exception, e:
+        print 'Exception: %s' % str(e)
+
+    # open up the output filename that matches the database name
+    path = './static/attr'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    outputFilename = path + '/' + competition + '.csv'
+    fo = open(outputFilename, "w+")
+
+    # Write out the column headers
+    mystring = 'Team,Score'
+    try:
+        for attr_def in attr_order:
+            if attr_def['Database_Store'] == 'Yes':
+                mystring += ',' + attr_def['Name']
+    except Exception, e:
+        print 'Exception: %s' % str(e)
+
+    mystring += '\n'
+    fo.write( mystring )
+    
+    # Get the list of teams in rank order, then loop through teams and dump their stored
+    # attribute values
+    team_rankings = getTeamsInRankOrder(session, competition)
+    for team_entry in team_rankings:
+        mystring = str(team_entry.team) + ',' + str(team_entry.score)
+        # retrieve each attribute from the database in the proper order
+        for attr_def in attr_order:
+            if attr_def['Database_Store'] == 'Yes':
+                attribute = getTeamAttribute(session, team_entry.team, competition, attr_def['Name'])
+                # if the attribute doesn't exist, just put in an empty field so that the columns
+                # stay aligned
+                if attribute == None:
+                    mystring += ','
+                elif ( attr_def['Statistic_Type'] == 'Total'):
+                    #mystring += ',' + str(attribute.cumulative_value)
+                    mystring += ',' + mapValueToString(attribute.cumulative_value, attribute.all_values, attr_def)
+                elif ( attr_def['Statistic_Type'] == 'Average'):
+                    #mystring += ',' + str(attribute.avg_value)
+                    mystring += ',' + mapValueToString(attribute.avg_value, attribute.all_values, attr_def)
+                else:
+                    #mystring += ',' + str(attribute.attr_value)
+                    mystring += ',' + mapValueToString(attribute.attr_value, attribute.all_values, attr_def)
+
+        mystring += '\n'
+        fo.write( mystring )
+    fo.close()
 
 # The run_test method contains just a bunch of little operations to test out how the various
 # database mechanisms work...    
