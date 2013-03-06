dd6aff394c696e273112d8cc82590f7e50be1e10
Ken St.Hilaire <kensthilaire@gmail.com>
2013-03-06 12:34:34 -0500

    expanded form to support the configuration of all global system settings

--- CentralApp/src/WebSetConfig.py
+++ CentralApp/src/WebSetConfig.py
@@ -8,33 +8,53 @@ from web import form
 
 # Form definition and callback class for the application configuration settings
 cfg_this_comp_label = "Competition:"
-cfg_db_master_label = "Database Master:"
 cfg_attr_defs_label = "Attributes Definitions File:"
+cfg_my_team_label = "My Team Number:"
+cfg_other_comp_label = "Other Competitions:"
+cfg_scouting_db_name_label = "Scouting Database Name:"
+cfg_debrief_db_name_label = "Debrief Database Name:"
+cfg_issues_db_name_label = "Issues Database Name:"
+cfg_issues_db_master_label = "Issues Database Master:"
 
-cfg_db_master_options = ['Yes', 'No']
+
+cfg_issues_db_master_options = ['Yes', 'No']
 
 cfgform = form.Form( 
+    form.Textbox(cfg_my_team_label, size=60),
     form.Textbox(cfg_this_comp_label, size=60),
+    form.Textbox(cfg_other_comp_label, size=60),
     form.Textbox(cfg_attr_defs_label, size=60),
-    form.Dropdown(cfg_db_master_label, cfg_db_master_options))
+    form.Textbox(cfg_scouting_db_name_label, size=60),
+    form.Textbox(cfg_issues_db_name_label, size=60),
+    form.Dropdown(cfg_issues_db_master_label, cfg_issues_db_master_options),
+    form.Textbox(cfg_debrief_db_name_label, size=60))
 
 def get_form(global_config):
     global_config['logger'].debug( 'GET Set Configuration Form' )
     
     form = cfgform()
+    form[cfg_my_team_label].value = global_config['my_team']
     form[cfg_this_comp_label].value = global_config['this_competition']
+    form[cfg_other_comp_label].value = global_config['other_competitions']
     form[cfg_attr_defs_label].value = global_config['attr_definitions']
-    form[cfg_db_master_label].value = global_config['issues_db_master']
+    form[cfg_scouting_db_name_label].value = global_config['db_name']
+    form[cfg_issues_db_name_label].value = global_config['issues_db_name']
+    form[cfg_issues_db_master_label].value = global_config['issues_db_master']
+    form[cfg_debrief_db_name_label].value = global_config['debriefs_db_name']
         
     return form
 
 def process_form(global_config, form):
     global_config['logger'].debug( 'Process Set Configuration Form' )
 
+    global_config['my_team'] = form[cfg_my_team_label].value
     global_config['this_competition'] = form[cfg_this_comp_label].value
+    global_config['other_competitions'] = form[cfg_other_comp_label].value
     global_config['attr_definitions'] = form[cfg_attr_defs_label].value
     
-    global_config['issues_db_master'] = form[cfg_db_master_label].value
-    
+    global_config['db_name'] = form[cfg_scouting_db_name_label].value
+    global_config['issues_db_name'] = form[cfg_issues_db_name_label].value
+    global_config['issues_db_master'] = form[cfg_issues_db_master_label].value
+    global_config['debriefs_db_name'] = form[cfg_debrief_db_name_label].value
     
     
