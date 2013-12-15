579922b81d7a31d9123ac5bd5c7d7904a439597f
Ken St.Hilaire <kensthilaire@gmail.com>
2013-12-14 20:16:32 -0500

    Consolidated the images and css files under the media directory
    Moved references to some images and css filed under the media directory.
    Need to check with Evin to see if this will now conflict with some of
    the changes that he has done.

--- CentralApp/src/WebCommonUtils.py
+++ CentralApp/src/WebCommonUtils.py
@@ -4,8 +4,8 @@ def get_html_head(title_str = 'FIRST Team 1073 - The Force Team'):
     head_str += '<meta charset="utf-8" />'
     head_str += '<title>%s</title>' % title_str
     head_str += '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1, minimum-scale=0.0, maximum-scale=2.0" />'
-    head_str += '<link rel="shortcut icon" href="/static/images/favicon.ico" type="image/x-icon" />'
-    head_str += '<link rel="stylesheet" href="/static/css/style.css" type="text/css" media="screen" />'
+    head_str += '<link rel="shortcut icon" href="/static/media/images/1073-favicon.ico" type="image/x-icon" />'
+    head_str += '<link rel="stylesheet" href="/static/media/css/style.css" type="text/css" media="screen" />'
 
     head_str += '	<style type="text/css" title="currentStyle">'
     head_str += '		@import "/static/media/css/demo_page.css";'
