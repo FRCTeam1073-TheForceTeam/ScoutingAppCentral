da93eda48ee553fb414ddfea41b3d6259b07e8ae
Ken St.Hilaire <kensthilaire@gmail.com>
2014-03-04 15:36:26 -0500

    Added support to download apk files from the central server
    As a way to simplify the update of the scouting application on the
    tablets, added the ability for the tablets to pull any APK files that
    are present in a specific directory on the server.

--- CentralApp/src/FileSync.py
+++ CentralApp/src/FileSync.py
@@ -23,6 +23,9 @@ def get_file(path):
     if path.find('.jpg') != -1:
         fd = open(path, 'rb')
         file_data = fd.read()
+    elif path.find('.apk') != -1:
+        fd = open(path, 'rb')
+        file_data = fd.read()
     else:
         file_data = ''
         fd = open(path, 'r')
