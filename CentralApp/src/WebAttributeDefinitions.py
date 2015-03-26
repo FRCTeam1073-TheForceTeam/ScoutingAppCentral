4150f8a643aeee18f99245c5e426d7edbaf164f1
Ken St.Hilaire <kensthilaire@gmail.com>
2015-03-25 23:10:23 -0400

    Added formatting of JSON attribute tree
    Added a new function to generate a JSON formatted string containing the
    attribute tree, suitable for dhtmlxTree.

--- CentralApp/src/WebAttributeDefinitions.py
+++ CentralApp/src/WebAttributeDefinitions.py
@@ -70,6 +70,51 @@ def process_attr_def_form(global_config, form):
             DataModel.recalculate_scoring(global_config, competition, attr_definitions)
 
 
+def get_attr_tree_json(global_config):
+    
+    global_config['logger'].debug( 'GET Attribute Definitions Tree JSON' )
+
+    attrdef_filename = './config/' + global_config['attr_definitions']
+    attr_definitions = AttributeDefinitions.AttrDefinitions()
+    attr_definitions.parse(attrdef_filename)
+    
+    #categories = attr_definitions.get_sub_categories()
+    
+    result = []
+    result.append('{ "item": [\n')
+    
+    checked = 0
+    opened = 1
+    result.append('    { "text": "%s", "open": %d, "checked": %d, "id": "Skip_%s", "item": [ \n' % ('All Attributes',opened,checked,'All'))
+    
+    category_dict = attr_definitions.get_attr_dict_by_category()
+    for category, attrlist in category_dict.iteritems():
+        if category != 'Uncategorized':
+            result.append('    { "text": "%s", "checked": %d, "id": "Skip_%s", "item": [ \n' % (category,checked,category))
+            for attrname in sorted(attrlist):
+                result.append('        { "text": "%s", "checked": %d, "id": "%s", "item": [ ] }' % (attrname,checked,attrname))
+                result.append(',\n')
+                          
+            if len(attrlist) > 0:
+                result = result[:-1]
+                result.append('\n')
+
+            result.append( '    ] }')
+            result.append(',\n')
+            
+    attrlist = category_dict['Uncategorized']
+    if len( attrlist ) > 0:
+        for attrname in sorted(attrlist):
+            result.append('    { "text": "%s", "checked": %d, "id": "%s", "item": [ ] }' % (attrname,checked,attrname))
+            result.append(',\n')
+                      
+    result = result[:-1]
+    result.append('],\n    "id": 1 \n}\n')
+    result.append('],\n    "id": 0 \n}\n')
+                                 
+    json_str = ''.join(result)
+
+    return json_str
 
 
 
